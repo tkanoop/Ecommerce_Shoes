ddada1ffffecd5beee21f13efa2198ee3c61866e
Anoop tk <anooptk3@gmail.com>
2023-11-10 00:00:35 +0530

    icons added

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,10 +1,32 @@
 
+import {FiHeart} from 'react-icons/fi'
+import {AiOutlineShoppingCart,AiOutlineUserAdd} from 'react-icons/fi'
 
 function Nav(){
     return (
-        <div>
-            nav
+       <nav>
+        <div className="nav-container">
+            <input type="text"
+            className="search-input"
+            placeholder="enter your search term"
+            />
+
         </div>
+        <div className="profile-container">
+            <a href="#">
+                <FiHeart className='nav-icons'/>
+
+
+            </a>
+            <a href='#'>
+               
+            </a>
+            <a href='#'>
+                
+            </a>
+        </div>
+
+       </nav>
     )
 }
 export default Nav
